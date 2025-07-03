2ab6f66e2e1d4244d7a1e9086bd930c5795a3dd3
GIYEOL <bgy11178@gmail.com>
2025-07-03 10:41:13 +0900

    d

--- chap12-jenkins/src/main/java/org/example/jenkins/controller/CalculatorController.java
+++ chap12-jenkins/src/main/java/org/example/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive!😁";
+        return "I'm alive!😁diediedie";
     }
 
     @GetMapping("/plus")
